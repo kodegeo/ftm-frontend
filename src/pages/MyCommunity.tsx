15b2c32bb3084e55f9c599aad853353757f3b0a3
Khalid Morris <kha.morris@showgeo.com>
2025-12-01 17:02:55 -0700

    Update Community page

--- src/pages/MyCommunity.tsx
+++ src/pages/MyCommunity.tsx
@@ -3,7 +3,7 @@ import { CommunityHero } from "../components/community/CommunityHero"
 import { FeatureCards } from "../components/community/FeatureCards"
 import { EventsSection } from "../components/community/EventsSection"
 import { FeedbackSection } from "../components/community/FeedbackSection"
-import { ProgressUpdates } from "../components/community/ProgressUpdates"
+//  import { ProgressUpdates } from "../components/community/ProgressUpdates"
 import { HealthInitiatives } from "../components/community/HealthInitiatives"
 import { NewsletterStrip } from "../components/community/NewsletterStrip"
 
